910524b484fc4b713d3a4eb326cc56c69e46d483
kalpasuraweera <kalpamadhushan18@gmail.com>
2023-11-11 10:39:43 +0530

    fix instructor

--- instructor/instructor.php
+++ instructor/instructor.php
@@ -29,28 +29,51 @@ if (isset($_POST["logout"])) {
         <h2 class="text-2xl font-semibold mb-4">View and Update Personal Info</h2>
         <div class="border p-4 rounded-md">
             <!-- Instructor Info Form -->
-            <form action="" method="post">
-                <label for="instructor_id">Instructor ID:</label>
-                <input type="text" name="instructor_id" id="instructor_id">
-                <label for="name">Name:</label>
-                <input type="text" name="name" id="name">
-                <label for="dob">Date of Birth:</label>
-                <input type="date" name="dob" id="dob">
-                <button type="submit" name="update_info">Update Info</button>
-            </form>
+            <form action="" method="post" class="mb-4">
+                <label for="instructor_id" class="block text-lg font-semibold">Instructor ID:</label>
+                <input type="text" name="instructor_id" id="instructor_id" class="border p-2 rounded-md" required>
 
+                <label for="name" class="block text-lg font-semibold mt-4">Name:</label>
+                <input type="text" name="name" id="name" class="border p-2 rounded-md" required>
+
+                <button type="submit" name="update_info" class="bg-blue-500 text-white px-4 py-2 rounded-md mt-4">Update
+                    Info</button>
+            </form>
             <?php
+
+            if (isset($_POST["update_info"])) {
+                // Get the instructor ID from the form submission
+                $instructor_id = $_POST["instructor_id"];
+                $name = $_POST["name"];
+
+                // Update the instructor's personal information in the database
+                $query = "UPDATE instructor SET name = '$name' WHERE instructor_id = '$instructor_id'";
+                $result = $conn->query($query);
+
+                if ($result) {
+                    echo "Personal information updated successfully.";
+                } else {
+                    echo "Error updating personal information: " . $conn->error;
+                }
+            }
+
             // Display a table of all instructor details
-            $query = "SELECT * FROM instructor";
+            $query = "SELECT i.instructor_id, i.name, ic.contact, isu.subject, d.department_name 
+            FROM instructor i 
+            LEFT JOIN instructor_contact ic ON i.instructor_id = ic.instructor_id 
+            LEFT JOIN instructor_subject isu ON i.instructor_id = isu.instructor_id 
+            LEFT JOIN department d ON d.department_id = i.department_id";
             $result = $conn->query($query);
 
             if ($result && $result->num_rows > 0) {
-                echo "<h3>All Instructors:</h3>";
+                echo "<p class='text-lg font-semibold mb-4'><strong>Student Information:</strong></p>";
                 echo "<table class='table-auto border border-collapse border-gray-700'>";
                 echo "<thead class='bg-gray-300'>";
                 echo "<tr>";
                 echo "<th class='px-4 py-2'>Instructor ID</th>";
                 echo "<th class='px-4 py-2'>Name</th>";
+                echo "<th class='px-4 py-2'>Contact</th>";
+                echo "<th class='px-4 py-2'>Subject</th>";
                 echo "<th class='px-4 py-2'>Department</th>";
                 echo "</tr>";
                 echo "</thead>";
@@ -60,7 +83,9 @@ if (isset($_POST["logout"])) {
                     echo "<tr>";
                     echo "<td class='border px-4 py-2'>" . $row["instructor_id"] . "</td>";
                     echo "<td class='border px-4 py-2'>" . $row["name"] . "</td>";
-                    echo "<td class='border px-4 py-2'>" . $row["department_id"] . "</td>";
+                    echo "<td class='border px-4 py-2'>" . $row["contact"] . "</td>";
+                    echo "<td class='border px-4 py-2'>" . $row["subject"] . "</td>";
+                    echo "<td class='border px-4 py-2'>" . $row["department_name"] . "</td>";
                     echo "</tr>";
                 }
 
@@ -70,22 +95,8 @@ if (isset($_POST["logout"])) {
                 echo "No instructor details available.";
             }
 
-            if (isset($_POST["update_info"])) {
-                // Get the instructor ID from the form submission
-                $instructor_id = $_POST["instructor_id"];
-                $name = $_POST["name"];
-                $dob = $_POST["dob"];
 
-                // Update the instructor's personal information in the database
-                $query = "UPDATE instructor SET name = '$name', dob = '$dob' WHERE instructor_id = '$instructor_id'";
-                $result = $conn->query($query);
 
-                if ($result) {
-                    echo "Personal information updated successfully.";
-                } else {
-                    echo "Error updating personal information: " . $conn->error;
-                }
-            }
             ?>
         </div>
     </section>
@@ -216,10 +227,11 @@ if (isset($_POST["logout"])) {
         <h2 class="text-2xl font-semibold mb-4">View Teaching History</h2>
         <div class="border p-4 rounded-md">
             <!-- Instructor ID Form -->
-            <form action="view_teaching_history.php" method="post">
-                <label for="instructor_id">Instructor ID:</label>
-                <input type="text" name="instructor_id" id="instructor_id">
-                <button type="submit" name="view_history">View History</button>
+            <form action="" method="post" class="mb-4">
+                <label for="instructor_id" class="block text-lg font-semibold">Instructor ID:</label>
+                <input type="text" name="instructor_id" id="instructor_id" class="border p-2 rounded-md" required>
+                <button type="submit" name="view_history" class="bg-blue-500 text-white px-4 py-2 rounded-md mt-4">View
+                    History</button>
             </form>
 
             <?php
@@ -227,16 +239,34 @@ if (isset($_POST["logout"])) {
                 // Get the instructor ID from the form submission
                 $instructor_id = $_POST["instructor_id"];
 
-                // Retrieve teaching history from the database based on the provided instructor ID
-                $query = "SELECT * FROM teaching WHERE instructor_id = '$instructor_id'";
+                // Retrieve teaching history with course names from the database based on the provided instructor ID
+                $query = "SELECT t.*, c.title AS course_name
+                      FROM teaching t
+                      JOIN course c ON t.course_code = c.course_code
+                      WHERE t.instructor_id = '$instructor_id'";
                 $result = $conn->query($query);
 
                 if ($result && $result->num_rows > 0) {
-                    echo "<table>";
-                    echo "<tr><th>Course Code</th><th>Teaching Semester</th></tr>";
+                    echo "<p class='text-lg font-semibold mb-4'><strong>Teaching History:</strong></p>";
+                    echo "<table class='table-auto border border-collapse border-gray-700'>";
+                    echo "<thead class='bg-gray-300'>";
+                    echo "<tr>";
+                    echo "<th class='px-4 py-2'>Course Code</th>";
+                    echo "<th class='px-4 py-2'>Course Name</th>";
+                    echo "<th class='px-4 py-2'>Teaching Semester</th>";
+                    echo "</tr>";
+                    echo "</thead>";
+                    echo "<tbody>";
+
                     while ($row = $result->fetch_assoc()) {
-                        echo "<tr><td>" . $row['course_code'] . "</td><td>" . $row['teaching_semester'] . "</td></tr>";
+                        echo "<tr>";
+                        echo "<td class='border px-4 py-2'>" . $row['course_code'] . "</td>";
+                        echo "<td class='border px-4 py-2'>" . $row['course_name'] . "</td>";
+                        echo "<td class='border px-4 py-2'>" . $row['semester_id'] . "</td>";
+                        echo "</tr>";
                     }
+
+                    echo "</tbody>";
                     echo "</table>";
                 } else {
                     echo "No teaching history available for instructor ID: $instructor_id";
@@ -247,19 +277,24 @@ if (isset($_POST["logout"])) {
     </section>
 
 
+
     <!-- Section 2: Assign Grades to Students in Courses -->
     <section class="my-8">
         <h2 class="text-2xl font-semibold mb-4">Assign Grades to Students in Courses</h2>
         <div class="border p-4 rounded-md">
             <!-- Grade Assignment Form -->
             <form action="" method="post">
-                <label for="student_id">Student ID:</label>
-                <input type="text" name="student_id" id="student_id">
-                <label for="course_code">Course Code:</label>
-                <input type="text" name="course_code" id="course_code">
-                <label for="grade">Grade:</label>
-                <input type="text" name="grade" id="grade">
-                <button type="submit" name="assign_grade">Assign Grade</button>
+                <label for="student_id" class="block text-lg font-semibold">Student ID:</label>
+                <input type="text" name="student_id" id="student_id" class="border p-2 rounded-md" required>
+
+                <label for="course_code" class="block text-lg font-semibold mt-4">Course Code:</label>
+                <input type="text" name="course_code" id="course_code" class="border p-2 rounded-md" required>
+
+                <label for="grade" class="block text-lg font-semibold mt-4">Grade:</label>
+                <input type="text" name="grade" id="grade" class="border p-2 rounded-md" required>
+
+                <button type="submit" name="assign_grade"
+                    class="bg-blue-500 text-white px-4 py-2 rounded-md mt-4">Assign Grade</button>
             </form>
 
             <?php
@@ -273,36 +308,49 @@ if (isset($_POST["logout"])) {
                 $result = $conn->query($query);
 
                 if ($result) {
-                    echo "Grade assigned successfully.";
+                    echo "<p class='mt-4 text-green-500'>Grade assigned successfully.</p>";
                 } else {
-                    echo "Error assigning grade: " . $conn->error;
+                    echo "<p class='mt-4 text-red-500'>Error assigning grade: " . $conn->error . "</p>";
                 }
             }
             ?>
 
-            <h3>Available Grades:</h3>
-            <table class="table-auto border border-collapse border-gray-700">
+            <p class='text-lg font-semibold mb-4'><strong>Available Grades:</strong></p>
+            <table class="table-auto border border-collapse border-gray-700 mt-2">
                 <thead class="bg-gray-300">
                     <tr>
+                        <th class="px-4 py-2">Student ID</th>
+                        <th class="px-4 py-2">Course Code</th>
+                        <th class="px-4 py-2">Instructor ID</th>
                         <th class="px-4 py-2">Grade Value</th>
+                        <th class="px-4 py-2">Date</th>
+                        <!-- You can add more columns here as needed -->
                     </tr>
                 </thead>
                 <tbody>
                     <?php
                     // Retrieve and display all available grades
-                    $query = "SELECT DISTINCT grade_value FROM grade";
+                    $query = "SELECT * FROM grade";
                     $result = $conn->query($query);
 
                     if ($result && $result->num_rows > 0) {
                         while ($row = $result->fetch_assoc()) {
-                            echo "<tr><td class='border px-4 py-2'>" . $row['grade_value'] . "</td></tr>";
+                            echo "<tr>";
+                            echo "<td class='border px-4 py-2'>" . $row['student_id'] . "</td>";
+                            echo "<td class='border px-4 py-2'>" . $row['course_code'] . "</td>";
+                            echo "<td class='border px-4 py-2'>" . $row['instructor_id'] . "</td>";
+                            echo "<td class='border px-4 py-2'>" . $row['grade_value'] . "</td>";
+                            echo "<td class='border px-4 py-2'>" . $row['date'] . "</td>";
+                            // You can add more cells for additional columns here
+                            echo "</tr>";
                         }
                     } else {
-                        echo "<tr><td class='border px-4 py-2' colspan='1'>No grades available.</td></tr>";
+                        echo "<tr><td class='border px-4 py-2' colspan='5'>No grades available.</td></tr>";
                     }
                     ?>
                 </tbody>
             </table>
+
         </div>
     </section>
 
@@ -314,15 +362,20 @@ if (isset($_POST["logout"])) {
         <div class="border p-4 rounded-md">
             <!-- Course Materials Form -->
             <form action="" method="post">
-                <label for="material_title">Material Title:</label>
-                <input type="text" name="material_title" id="material_title">
-                <label for="material_format">Material Format:</label>
-                <input type="text" name="material_format" id="material_format">
-                <label for="material_link">Material Link:</label>
-                <input type="text" name="material_link" id="material_link">
-                <label for="course_code">Course Code:</label>
-                <input type="text" name="course_code" id="course_code">
-                <button type="submit" name="add_material">Add Material</button>
+                <label for="material_title" class="block text-lg font-semibold">Material Title:</label>
+                <input type="text" name="material_title" id="material_title" class="border p-2 rounded-md" required>
+
+                <label for="material_format" class="block text-lg font-semibold mt-4">Material Format:</label>
+                <input type="text" name="material_format" id="material_format" class="border p-2 rounded-md" required>
+
+                <label for="material_link" class="block text-lg font-semibold mt-4">Material Link:</label>
+                <input type="text" name="material_link" id="material_link" class="border p-2 rounded-md" required>
+
+                <label for="course_code" class="block text-lg font-semibold mt-4">Course Code:</label>
+                <input type="text" name="course_code" id="course_code" class="border p-2 rounded-md" required>
+
+                <button type="submit" name="add_material" class="bg-blue-500 text-white px-4 py-2 rounded-md mt-4">Add
+                    Material</button>
             </form>
 
             <?php
@@ -338,14 +391,43 @@ if (isset($_POST["logout"])) {
                 $result = $conn->query($query);
 
                 if ($result) {
-                    echo "Course material added successfully.";
+                    echo "<p class='mt-4 text-green-500'>Course material added successfully.</p>";
                 } else {
-                    echo "Error adding course material: " . $conn->error;
+                    echo "<p class='mt-4 text-red-500'>Error adding course material: " . $conn->error . "</p>";
                 }
             }
             ?>
+        </div>
+    </section>
 
-            <!-- You can add additional code here to display a list of added course materials if needed. -->
+    <!-- Section 5: Delete Course Material -->
+    <section class="my-8">
+        <h2 class="text-2xl font-semibold mb-4">Delete Course Material</h2>
+        <div class="border p-4 rounded-md">
+            <!-- Course Material Deletion Form -->
+            <form action="" method="post">
+                <label for="material_id" class="block text-lg font-semibold">Material ID:</label>
+                <input type="text" name="material_id" id="material_id" class="border p-2 rounded-md" required>
+
+                <button type="submit" name="delete_material"
+                    class="bg-red-500 text-white px-4 py-2 rounded-md mt-4">Delete Material</button>
+            </form>
+
+            <?php
+            if (isset($_POST["delete_material"])) {
+                $material_id = $_POST["material_id"];
+
+                // Delete the course material from the database
+                $query = "DELETE FROM course_material WHERE material_id = '$material_id'";
+                $result = $conn->query($query);
+
+                if ($result) {
+                    echo "<p class='mt-4 text-green-500'>Course material deleted successfully.</p>";
+                } else {
+                    echo "<p class='mt-4 text-red-500'>Error deleting course material: " . $conn->error . "</p>";
+                }
+            }
+            ?>
         </div>
     </section>
 
@@ -354,10 +436,11 @@ if (isset($_POST["logout"])) {
         <h2 class="text-2xl font-semibold mb-4">Access Course Materials</h2>
         <div class="border p-4 rounded-md">
             <!-- Instructor ID Form -->
-            <form action="" method="post">
-                <label for="instructor_id">Instructor ID:</label>
-                <input type="text" name="instructor_id" id="instructor_id">
-                <button type="submit" name="search_materials">Search</button>
+            <form action="" method="post" class="mb-4">
+                <label for="instructor_id" class="block text-lg font-semibold">Instructor ID:</label>
+                <input type="text" name="instructor_id" id="instructor_id" class="border p-2 rounded-md" required>
+                <button type="submit" name="search_materials"
+                    class="bg-blue-500 text-white px-4 py-2 rounded-md mt-4">Search</button>
             </form>
 
             <?php
@@ -389,7 +472,7 @@ INNER JOIN instructor i ON cm.instructor_id = i.instructor_id";
                     echo "<td class='border px-4 py-2'>" . $row["material_id"] . "</td>";
                     echo "<td class='border px-4 py-2'>" . $row["title"] . "</td>";
                     echo "<td class='border px-4 py-2'>" . $row["format"] . "</td>";
-                    echo "<td class='border px-4 py-2 text-red-800'><a href='" . $row["link"] . "'>Download</a></td>";
+                    echo "<td class='border px-4 py-2 text-blue-500'><a href='" . $row["link"] . "' target='_blank'>Download</a></td>";
                     echo "<td class='border px-4 py-2'>" . $row["course_name"] . "</td>";
                     echo "<td class='border px-4 py-2'>" . $row["instructor_name"] . "</td>";
                     echo "</tr>";
@@ -398,12 +481,13 @@ INNER JOIN instructor i ON cm.instructor_id = i.instructor_id";
                 echo "</tbody>";
                 echo "</table>";
             } else {
-                echo "No course materials found.";
+                echo "<p class='mt-4 text-red-500'>No course materials found.</p>";
             }
             ?>
 
         </div>
     </section>
+
 
 
     <?php
